ab0cef44f5e64960cfb2999cd2bb6f177cd47fd6
fuchs04 <akos.toth@pludoni.de>
2012-04-02 13:16:20 +0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  root to: 'pages#home'
   match '/contact', to: 'pages#contact'
   match '/about', to: 'pages#about'
   match '/help', to: 'pages#help'
+  match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,8 +55,6 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root to: 'pages#home'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
